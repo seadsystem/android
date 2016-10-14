0688968af3f20b0bc761f9efc2744a49fa000cb0
Christopher Persons <christopherpersons@Christophers-MacBook-Pro.local>
2016-10-13 18:35:22 -0700

    Splash Screen

---
+++ SEADS/app/src/main/java/android/sead_systems/seads/SplashActivity.java
@@ -0,0 +1,18 @@
+package android.sead_systems.seads;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // TODO call this to move to next activity with proper .class
+        //Intent intent = new Intent(this, MainActivity.class);
+        //startActivity(intent);
+        //finish();
+    }
+}
