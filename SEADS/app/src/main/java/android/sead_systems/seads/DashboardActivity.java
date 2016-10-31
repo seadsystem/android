8cf4c87f23ff8fbf447ecb1c5e3ab181b028c478
Chris-Persons <chrispersons@gmail.com>
2016-10-30 21:16:20 -0700

    Finished gridView

--- SEADS/app/src/main/java/android/sead_systems/seads/DashboardActivity.java
+++ SEADS/app/src/main/java/android/sead_systems/seads/DashboardActivity.java
@@ -45,74 +45,20 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
 
-       /* // FIXME: This is DEBUG code meant only for the demo. Remove this!
-        TextView textView = (TextView) findViewById(R.id.currentUsageTextbox);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), TemperatureActivity.class);
-                startActivity(intent);
-
-            }
-        });
-
-
-        //get rid of title replace with seads logo
-        setTitle("");
-        */
-        // getSupportActionBar().setIcon(R.mipmap.logo);
-        //populateListView();
-        //registerOnClick();
-
-    }
-    /*
-    private void populateListView() {
-        //can adapt this list to be only which devices the user wants to see
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.devicelist, DeviceManagerFactory.getInstance().generateListOfDevices() );
-        ListView list = (ListView) findViewById(R.id.listView);
-        list.setAdapter(adapter);
-
-        ((TextView) findViewById(R.id.currentUsageTextbox)).setText(String.valueOf(
-                DeviceManagerFactory.getInstance().generateCurrentPowerUsage()));
-        //CustomList will be used when we put pictures with the text on the list, will have to redo adapter
-
-        //CustomList adapter1 = new
-        //CustomList adapter1 = new CustomList(DashboardActivity.this,devices,R.drawable.test);
-        //list=(ListView)findViewById(R.id.list);
-        //list.setAdapter(adapter);
     }
 
-    //This function will be used to run an individual fragment based on which item was clicked
-    private void registerOnClick() {
-        ListView list = (ListView) findViewById(R.id.listView);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            //put method to run graph fragment here
-            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
-            }
-        });
-    }
-
-    */
     private final class MyAdapter extends BaseAdapter {
         private final List<Item> mItems = new ArrayList<Item>();
         private final LayoutInflater mInflater;
 
         public MyAdapter(Context context) {
             mInflater = LayoutInflater.from(context);
-
-            mItems.add(new Item("1",        R.mipmap.logo));
-            mItems.add(new Item("2",        R.mipmap.logo));
-            mItems.add(new Item("3",        R.mipmap.logo));
-            mItems.add(new Item("4",        R.mipmap.logo));
-            mItems.add(new Item("5",        R.mipmap.logo));
-            mItems.add(new Item("6",        R.mipmap.logo));
-            mItems.add(new Item("7",        R.mipmap.logo));
-            mItems.add(new Item("8",        R.mipmap.logo));
-            mItems.add(new Item("9",        R.mipmap.logo));
-            mItems.add(new Item("10",       R.mipmap.logo));
-            mItems.add(new Item("11",       R.mipmap.logo));
-            mItems.add(new Item("12",       R.mipmap.logo));
+            mItems.add(new Item("Master Bedroom",       R.mipmap.bedroom1));
+            mItems.add(new Item("Guest Bedroom",        R.mipmap.bedroom2));
+            mItems.add(new Item("Living Room ",         R.mipmap.livingroom));
+            mItems.add(new Item("Office",               R.mipmap.office));
+            mItems.add(new Item("Master Bathroom",      R.mipmap.bathroom));
+            mItems.add(new Item("Kitchen",              R.mipmap.kitchen));
         }
 
         @Override
@@ -164,5 +110,3 @@ public class DashboardActivity extends AppCompatActivity {
         }
     }
 }
-
-
