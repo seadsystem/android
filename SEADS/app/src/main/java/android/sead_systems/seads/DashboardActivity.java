a4cbab2dd82eb005b48e1536a0d59b488db92abd
Chris-Persons <chrispersons@gmail.com>
2016-10-30 18:40:32 -0700

    Grid View with onclick for items

--- SEADS/app/src/main/java/android/sead_systems/seads/DashboardActivity.java
+++ SEADS/app/src/main/java/android/sead_systems/seads/DashboardActivity.java
@@ -1,14 +1,23 @@
 package android.sead_systems.seads;
 
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.sead_systems.seads.devices.DeviceManagerFactory;
+import android.sead_systems.seads.graph.DemoActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DashboardActivity extends AppCompatActivity {
@@ -18,7 +27,25 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // FIXME: This is DEBUG code meant only for the demo. Remove this!
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(new MyAdapter(this));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // Gets the Item's Name and displays to the user currently
+                TextView tv = (TextView) view.findViewById(R.id.text);
+                Toast toast = Toast.makeText(getApplicationContext(), tv.getText(),
+                        Toast.LENGTH_SHORT);
+                toast.show();
+
+                // TODO Need function to take the name and populate the next activity based off
+                // TODO the current item's name that is clicked.
+                Intent intent = new Intent(getApplicationContext(), DemoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+       /* // FIXME: This is DEBUG code meant only for the demo. Remove this!
         TextView textView = (TextView) findViewById(R.id.currentUsageTextbox);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,12 +59,13 @@ public class DashboardActivity extends AppCompatActivity {
 
         //get rid of title replace with seads logo
         setTitle("");
+        */
         // getSupportActionBar().setIcon(R.mipmap.logo);
-        populateListView();
-        registerOnClick();
+        //populateListView();
+        //registerOnClick();
 
     }
-
+    /*
     private void populateListView() {
         //can adapt this list to be only which devices the user wants to see
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.devicelist, DeviceManagerFactory.getInstance().generateListOfDevices() );
@@ -48,10 +76,10 @@ public class DashboardActivity extends AppCompatActivity {
                 DeviceManagerFactory.getInstance().generateCurrentPowerUsage()));
         //CustomList will be used when we put pictures with the text on the list, will have to redo adapter
 
-        /*CustomList adapter1 = new
-        CustomList adapter1 = new CustomList(DashboardActivity.this,devices,R.drawable.test);
-        list=(ListView)findViewById(R.id.list);
-        list.setAdapter(adapter); */
+        //CustomList adapter1 = new
+        //CustomList adapter1 = new CustomList(DashboardActivity.this,devices,R.drawable.test);
+        //list=(ListView)findViewById(R.id.list);
+        //list.setAdapter(adapter);
     }
 
     //This function will be used to run an individual fragment based on which item was clicked
@@ -64,4 +92,77 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
     }
+
+    */
+    private final class MyAdapter extends BaseAdapter {
+        private final List<Item> mItems = new ArrayList<Item>();
+        private final LayoutInflater mInflater;
+
+        public MyAdapter(Context context) {
+            mInflater = LayoutInflater.from(context);
+
+            mItems.add(new Item("1",        R.mipmap.logo));
+            mItems.add(new Item("2",        R.mipmap.logo));
+            mItems.add(new Item("3",        R.mipmap.logo));
+            mItems.add(new Item("4",        R.mipmap.logo));
+            mItems.add(new Item("5",        R.mipmap.logo));
+            mItems.add(new Item("6",        R.mipmap.logo));
+            mItems.add(new Item("7",        R.mipmap.logo));
+            mItems.add(new Item("8",        R.mipmap.logo));
+            mItems.add(new Item("9",        R.mipmap.logo));
+            mItems.add(new Item("10",       R.mipmap.logo));
+            mItems.add(new Item("11",       R.mipmap.logo));
+            mItems.add(new Item("12",       R.mipmap.logo));
+        }
+
+        @Override
+        public int getCount() {
+            return mItems.size();
+        }
+
+        @Override
+        public Item getItem(int i) {
+            return mItems.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return mItems.get(i).drawableId;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View v = view;
+            ImageView picture;
+            TextView name;
+
+            if (v == null) {
+                v = mInflater.inflate(R.layout.grid_item, viewGroup, false);
+                v.setTag(R.id.picture, v.findViewById(R.id.picture));
+                v.setTag(R.id.text, v.findViewById(R.id.text));
+            }
+
+            picture = (ImageView) v.getTag(R.id.picture);
+            name = (TextView) v.getTag(R.id.text);
+
+            Item item = getItem(i);
+
+            picture.setImageResource(item.drawableId);
+            name.setText(item.name);
+
+            return v;
+        }
+
+        private class Item {
+            public final String name;
+            public final int drawableId;
+
+            Item(String name, int drawableId) {
+                this.name = name;
+                this.drawableId = drawableId;
+            }
+        }
+    }
 }
+
+
---
+++ SEADS/app/src/main/java/android/sead_systems/seads/SquareImageView.java
@@ -0,0 +1,29 @@
+package android.sead_systems.seads;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by christopherpersons on 10/30/16.
+ */
+
+public class SquareImageView extends ImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth()); //Snap to width
+    }
+}
