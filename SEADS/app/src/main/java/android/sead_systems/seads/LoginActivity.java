ac6f2bd7fec7c636427fc9ff15fb6218867436c7
talal.abouhaiba <talal.abouhaiba@gmail.com>
2016-11-08 17:54:30 -0800

    LoginActivity and SignupActivity now in working state.
    No server calls are being made.

--- SEADS/app/src/main/java/android/sead_systems/seads/LoginActivity.java
+++ SEADS/app/src/main/java/android/sead_systems/seads/LoginActivity.java
@@ -1,11 +1,14 @@
 package android.sead_systems.seads;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.sead_systems.seads.graph.DemoActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /** Created by talal.abouhaiba on 10/26/2016. */
 
@@ -17,24 +20,25 @@ public class LoginActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
 
-        Button loginButton = (Button)findViewById(R.id.loginbutton);
-        Button signupButton = (Button)findViewById(R.id.signupbutton);
-        findViewById(R.id.loginbutton).getBackground().setAlpha(0);
-        findViewById(R.id.signupbutton).getBackground().setAlpha(100);
+        Button loginButton = (Button)findViewById(R.id.button_login);
+        TextView signupButton = (TextView) findViewById(R.id.button_signup);
+
+        if (getIntent().getStringExtra("USERNAME") != null) {
+            ((EditText)findViewById(R.id.input_email)).setText(getIntent().getStringExtra("USERNAME"));
+            findViewById(R.id.input_password).requestFocus();
+        }
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
-                startActivity(intent);
-
+                login();
             }
         });
 
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(intent);
             }
@@ -45,4 +49,61 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    private void login() {
+
+        if (!validateInput()) {
+            return;
+        }
+
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Logging in");
+        progressDialog.setIndeterminate(true);
+        progressDialog.show();
+
+        Thread loginThread = new Thread() {
+            @Override
+            public void run() {
+                authenticateLogin();
+            }
+        };
+        loginThread.start();
+
+        progressDialog.dismiss();
+
+        Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
+        startActivity(intent);
+    }
+
+    private void authenticateLogin() {
+
+        /** Server call to be made here. **/
+
+    }
+
+    /**
+     * Validates the given email and password
+     * @return true if the email and password are correctly formed
+     */
+    private boolean validateInput() {
+        String emailInput = ((EditText)findViewById(R.id.input_email)).getText().toString();
+        String passwordInput = ((EditText)findViewById(R.id.input_password)).getText().toString();
+
+        if (emailInput.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            ((EditText)findViewById(R.id.input_email)).setError("Please enter a valid email address");
+            findViewById(R.id.input_email).requestFocus();
+            return false;
+        } else {
+            ((EditText)findViewById(R.id.input_email)).setError(null);
+        }
+
+        if (passwordInput.isEmpty()) {
+            ((EditText)findViewById(R.id.input_password)).setError("Please enter your password");
+            findViewById(R.id.input_password).requestFocus();
+            return false;
+        } else {
+            ((EditText)findViewById(R.id.input_password)).setError(null);
+        }
+
+        return true;
+    }
 }
--- SEADS/app/src/main/java/android/sead_systems/seads/SignUpActivity.java
+++ SEADS/app/src/main/java/android/sead_systems/seads/SignUpActivity.java
@@ -1,16 +1,115 @@
 package android.sead_systems.seads;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /** Created by talal.abouhaiba on 11/2/2016. */
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        TextView loginButton = (TextView)findViewById(R.id.button_login);
+        Button signupButton = (Button) findViewById(R.id.button_signup);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
     }
 
+    /**
+     * Removes transition when moving back to {@link LoginActivity}
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        overridePendingTransition(0,0);
+    }
+
+    private void signup() {
+        if (!validateInput()) {
+            return;
+        }
+
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Signing up");
+        progressDialog.setIndeterminate(true);
+        progressDialog.show();
+
+        Thread loginThread = new Thread() {
+            @Override
+            public void run() {
+                authenticateSignup();
+            }
+        };
+        loginThread.start();
+
+        progressDialog.dismiss();
+
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        intent.putExtra("USERNAME", ((EditText)findViewById(R.id.input_email)).getText().toString());
+        startActivity(intent);
+
+    }
+
+    private void authenticateSignup() {
+
+    }
+
+    private boolean validateInput() {
+        String nameInput = ((EditText)findViewById(R.id.input_name)).getText().toString();
+        String emailInput = ((EditText)findViewById(R.id.input_email)).getText().toString();
+        String passwordInput = ((EditText)findViewById(R.id.input_password)).getText().toString();
+
+        if (nameInput.isEmpty()) {
+            ((EditText)findViewById(R.id.input_name)).setError("Please enter your name");
+            findViewById(R.id.input_name).requestFocus();
+            return false;
+        } else {
+            ((EditText)findViewById(R.id.input_name)).setError(null);
+        }
+
+
+        if (emailInput.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            ((EditText)findViewById(R.id.input_email)).setError("Please enter a valid email address");
+            findViewById(R.id.input_email).requestFocus();
+            return false;
+        } else {
+            ((EditText)findViewById(R.id.input_email)).setError(null);
+        }
+
+        if (passwordInput.isEmpty()) {
+            ((EditText)findViewById(R.id.input_password)).setError("Please enter a valid password");
+            findViewById(R.id.input_password).requestFocus();
+            return false;
+        } else {
+            ((EditText)findViewById(R.id.input_password)).setError(null);
+        }
+
+        return true;
+
+    }
 }
