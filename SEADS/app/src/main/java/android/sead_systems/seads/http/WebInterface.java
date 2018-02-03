7addcbdbb1573e8e451954a9ee3feeef36418501
Kevin Jung <rubic22@gmail.com>
2018-02-03 00:17:37 -0800

    changed the documentation removed some useless comments

--- SEADS/app/src/main/java/android/sead_systems/seads/http/WebInterface.java
+++ SEADS/app/src/main/java/android/sead_systems/seads/http/WebInterface.java
@@ -10,6 +10,10 @@ import org.json.JSONObject;
 public interface WebInterface {
     /**
      * called from WebInterfacer when an HTTP response has been received
+     * The JSONObject result can be used in the following manner:
+     * JSONArray data = result.getJSONArray("data");
+     * JSONObject index0 = data.getJSONObject(0);
+     * Log.d("WebInterfacer", "time at index 0:" + index0.getString("time"));
      * @param result the result of the HTTP response
      */
     void onJSONRetrieved(JSONObject result);
--- SEADS/app/src/main/java/android/sead_systems/seads/http/WebInterfacer.java
+++ SEADS/app/src/main/java/android/sead_systems/seads/http/WebInterfacer.java
@@ -18,39 +18,16 @@ import java.net.URL;
  * 2. call getJSONObject() with the appropriate paramaters
  * 3. Inside the instantiating class, override
  */
+
+
 public class WebInterfacer implements HTTPGetRequestAsyncTask.HTTPResponse {
-    // TODO: figure out how we're gonna get the htpp response to the instantiator
-        // We can do the same thing we did and keep a reference to the instantiator. create another interface?
 
-    /*
-    TODO: Create methods to:
-        Make a http request with some paramaters
-        take in times, panels, granularity, type, should check for correctness
-
-     */
     private WebInterface instantiator;
 
     public WebInterfacer(WebInterface instantiator) {
         this.instantiator = instantiator;
     }
 
-//    public WebInterfacer() {
-//        try {
-//            URL url = getURL(1477395900, 1477395910, "energy", 1, 3, "P");
-////            URL url = new URL("http://db.sead.systems:8080/466419818?start_time=1477395900&"+
-////                    "end_time=1477395910&list_format=energy&granularity=1&device=Panel3&type=P");
-//            // Test code to attempt to get a string from server
-//            new HTTPGetRequestAsyncTask(this).execute(url);
-//
-//
-//            JSONObject wholeString = new JSONObject("{ \"data\": [{ \"time\": \"2016-10-25 04:45:09\", \"energy\": \"0.000036388888888888888889\" },{ \"time\": \"2016-10-25 04:45:08\", \"energy\": \"0.000036666666666666666667\" },{ \"time\": \"2016-10-25 04:45:07\", \"energy\": \"0.000036666666666666666667\" },{ \"time\": \"2016-10-25 04:45:06\", \"energy\": \"0.000036388888888888888889\" },{ \"time\": \"2016-10-25 04:45:05\", \"energy\": \"0.000036666666666666666667\" },{ \"time\": \"2016-10-25 04:45:04\", \"energy\": \"0.000036111111111111111111\" },{ \"time\": \"2016-10-25 04:45:03\", \"energy\": \"0.000036944444444444444444\" },{ \"time\": \"2016-10-25 04:45:02\", \"energy\": \"0.000036388888888888888889\" },{ \"time\": \"2016-10-25 04:45:01\", \"energy\": \"0.000036388888888888888889\" },{ \"time\": \"2016-10-25 04:45:00\" ,\"energy\": \"0.000036388888888888888889\" }]}");
-//            JSONArray data = wholeString.getJSONArray("data");
-//            JSONObject index0 = data.getJSONObject(0);
-//            Log.d("WebInterfacer", "time at index 0:" + index0.getString("time"));
-//        } catch(Exception e) {
-//            Log.d("WebInterfacer", "Failure.");
-//        }
-//    }
 
     /**
      * returns URL
