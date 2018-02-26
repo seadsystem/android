ebc879f9f43e0d43dd00c7b68b7b83117e755d83
prev work <prev.work@old.com>
2018-02-25 23:17:46 -0800

    old work + google auth code

---
+++ SEADS_V2/app/src/main/java/com/seads/seadsv2/WelcomeActivity.java
@@ -0,0 +1,192 @@
+package com.seads.seadsv2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Welcome activity to be displayed on first launch.
+ * @author Talal Abou Haiba
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private int[] mLayouts;
+
+    private ViewPager mViewPager;
+    private LinearLayout mDotsLayout;
+
+    private Button mNextButton;
+    private Button mSkipButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        setContentView(R.layout.activity_welcome);
+
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mDotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        mSkipButton = (Button) findViewById(R.id.btn_skip);
+        mNextButton = (Button) findViewById(R.id.btn_next);
+
+
+        mLayouts = new int[]{
+                R.layout.welcome_page1,
+                R.layout.welcome_page2,
+                R.layout.welcome_page3,
+                R.layout.welcome_page4};
+
+        addBottomDots(0);
+
+        // Set notification bar transparent
+        changeStatusBarColor();
+
+        mViewPager.setAdapter(new MyViewPagerAdapter());
+        mViewPager.addOnPageChangeListener(pageChangeListener);
+
+        mSkipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchHomeScreen();
+            }
+        });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = getItem(+1);
+                if (current < mLayouts.length) {
+                    // move to next screen
+                    mViewPager.setCurrentItem(current);
+                } else {
+                    launchHomeScreen();
+                }
+            }
+        });
+    }
+
+    private void addBottomDots(int currentPage) {
+        TextView[] dots = new TextView[mLayouts.length];
+
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+
+        mDotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorsInactive[currentPage]);
+            mDotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(colorsActive[currentPage]);
+    }
+
+    private int getItem(int i) {
+        return mViewPager.getCurrentItem() + i;
+    }
+
+    private void launchHomeScreen() {
+        getSharedPreferences(getString(R.string.shared_preferences), MODE_PRIVATE).edit().
+                putBoolean(getString(R.string.preference_first_time), false).apply();
+
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+        finish();
+    }
+
+    ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == mLayouts.length - 1) {
+                // last page. make button text to GOT IT
+                mNextButton.setText(getString(R.string.welcome_button_continue));
+                mSkipButton.setVisibility(View.GONE);
+            } else {
+                // still pages are left
+                mNextButton.setText(getString(R.string.welcome_button_next));
+                mSkipButton.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+    /**
+     * Sets the notification bar to be transparent
+     */
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(mLayouts[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return mLayouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+}
---
+++ SEADS_V2/app/src/main/java/com/seads/seadsv2/graph/TabFragment5.java
@@ -0,0 +1,212 @@
+package com.seads.seadsv2.graph;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import com.seads.seadsv2.R;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.support.v4.app.Fragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.Legend.LegendForm;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.components.YAxis.AxisDependency;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
+
+/*
+    Dymanic real-time updated chart fragment used in conjunction with DynamicChartActivity and
+    DemoBase.
+ */
+
+public class TabFragment5 extends Fragment implements View.OnClickListener {
+
+    LineChart mChart;
+    private boolean killMe = false;
+    private boolean running = false;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_fragment_5, container, false);
+        mChart = (LineChart) v.findViewById(R.id.chart1);
+
+        // enable description text
+        mChart.getDescription().setEnabled(false);
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
+
+        // set an alternative background color
+        mChart.setBackgroundColor(Color.WHITE);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.BLACK);
+
+        // add empty data
+        mChart.setData(data);
+        YAxis rightAxis = mChart.getAxisRight();
+        XAxis xAxis = mChart.getXAxis();
+        //xAxis.
+        rightAxis.setEnabled(false);
+
+        //set button
+        final Button button = (Button) v.findViewById(R.id.actionFeedMultiple);
+        button.setOnClickListener(this);
+        final Button button2 = (Button) v.findViewById(R.id.stopThread);
+        button2.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.actionFeedMultiple:
+                killMe = false;
+                if (running == true){
+
+                }
+                else {
+                    running = true;
+                    Context context = this.getContext();
+                    CharSequence text = "Connected to Live";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    feedMultiple();
+                }
+                break;
+
+            case R.id.stopThread:
+                killMe = true;
+                running = false;
+                Context context2 = this.getContext();
+                CharSequence text2 = "Disconnected";
+                int duration2 = Toast.LENGTH_SHORT;
+                Toast toast2 = Toast.makeText(context2, text2, duration2);
+                toast2.show();
+                stopUIThread();
+                break;
+        }
+    }
+
+    private void addEntry() {
+        LineData data = mChart.getData();
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+
+            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 40) + 30f), 0);
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            mChart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            mChart.setVisibleXRangeMaximum(120);
+            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            mChart.moveViewToX(data.getEntryCount());
+        }
+    }
+
+    private LineDataSet createSet() {
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setAxisDependency(AxisDependency.LEFT);
+        set.setColor(Color.GREEN);
+        set.setCircleColor(Color.BLACK);
+        set.setLineWidth(0.5f);
+        set.setCircleRadius(2f);
+        set.setFillAlpha(100);
+        set.setFillColor(ColorTemplate.getHoloBlue());
+        set.setHighLightColor(Color.rgb(0, 0, 0));
+        set.setValueTextColor(Color.BLACK);
+        set.setValueTextSize(1f);
+        set.setDrawValues(true);
+        return set;
+    }
+
+    private Thread thread;
+
+    private void feedMultiple() {
+
+        if (thread != null)
+            thread.interrupt();
+
+        final Runnable runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                addEntry();
+            }
+        };
+
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                //live feed continues will it is not stopped
+                while(!killMe) {
+                    // Don't generate garbage runnables inside the loop.
+                    getActivity().runOnUiThread(runnable);
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+                return;
+            }
+        });
+
+        thread.start();
+    }
+
+    private void stopUIThread(){
+        //thread.stop(); //do not use. kills UIthread and activity crash.
+    }
+
+}
